a5bdcb97d0a5658e9a3c2c0ca1279f17f1b9d45b
rocha <rocha.felipe98@gmail.com>
2026-07-23 14:51:23 -0300

    build: bump version to 1.5.0 (veiculos compartilhados em Meus Veiculos)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.flowfuel.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.4.1"
+        versionCode = 23
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
